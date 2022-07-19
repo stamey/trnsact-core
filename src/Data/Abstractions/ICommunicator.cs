6eb30450dc41a6bbda0be6ba187429cf521946ac
joelstamey <joelstamey@users.noreply.github.com>
2022-07-18 23:25:37 -0500

    Add data objects

---
+++ src/Data/Abstractions/ICommunicator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace trnsACT.Core.Data.Abstractions
+{
+    public interface ICommunicator
+    {
+        string CreatedBy { get; set; }
+        DateTimeOffset CreatedDate { get; set; }
+        string Description { get; set; }
+        ICollection<Models.Email> Email { get; set; }
+        string FromEmailAddress { get; set; }
+        string FromName { get; set; }
+        int Id { get; set; }
+        bool? IsActive { get; set; }
+        string Label { get; set; }
+        string LastChangedBy { get; set; }
+        DateTimeOffset LastChangedDate { get; set; }
+        string MailServerAddress { get; set; }
+        string MailServerPassword { get; set; }
+        short MailServerPort { get; set; }
+        string MailServerUsername { get; set; }
+        string Name { get; set; }
+        int ParentId { get; set; }
+        bool RequiresSecureConnection { get; set; }
+        string SecurityKey { get; set; }
+        string SecuritySecret { get; set; }
+        string Template { get; set; }
+    }
+}
---
+++ src/Data/Enums/MessageTypeId.cs
@@ -0,0 +1,14 @@
+namespace trnsACT.Core.Data
+{
+    public enum MessageTypeId
+    {
+        DefaultMessage = 1,
+        AllUsersMessage = 100,
+        RoleMessage = 200,
+        UserMessage = 300,
+        Announcement = 400,
+        Alert = 500,
+        EmailMessage = 600,
+        BusinessEmail = 700
+    }
+}
