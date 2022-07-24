0abb41ac7247312ef24f0fd0804764e80723b7e5
joelstamey <joelstamey@users.noreply.github.com>
2022-07-24 12:17:22 -0500

    Add problem types

--- src/Errors/ProblemTypes.cs
+++ src/Errors/ProblemTypes.cs
@@ -5,12 +5,15 @@
         public const string CompanyInactive = "CompanyInactive";
         public const string Forbidden = "Forbidden";
         public const string InvalidCode = "Invalid Code";
+        public const string InvalidCredentials = "Invalid Credentials";
+        public const string InvalidPassword = "Invalid Password";
         public const string InvalidToken = "Invalid Token";
         public const string Locked = "Locked";
+        public const string MissingInformation = "Missing Information"; 
         public const string ServerError = "ServerError";
         public const string ServiceCredentials = "Service credentials";
+        public const string UnapprovedUser = "Unapproved";
         public const string Unauthorized = "Unauthorized";
         public const string UnknownUser = "Unknown";
-        public const string UnapprovedUser = "Unapproved";
     }
 }
